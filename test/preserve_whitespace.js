a269b41722e1a5c5efd2d21cd9311e1c619eac2c
Chad Kirby <chad.kirby@gmail.com>
2023-05-25 16:38:21 -0700

    don't get fooled into thinking an abbreviation is a sentence boundary when it's not preceded by whitespace

--- test/preserve_whitespace.js
+++ test/preserve_whitespace.js
@@ -27,11 +27,25 @@ describe('Preserve whitespace', function () {
         var entry = "iv. determining that the advertisement in the lift study is a candidate ad for the user, computing whether to include the user in a test group or a control group for the lift study ([0032]), v. based on the computation indicating that the user is in the control group, holding out the advertisement from completing the ad selection process for the user ([0032]), and vi. based on the computation indicating that the user is in the test group, allowing the advertisement to continue through the ad selection process such that the user receives either the advertisement in the lift study or another advertisement ([0032]); and ";
         var sentences = tokenizer.sentences(entry, options);
 
-       it("should get the correct sentences", function () {
+        it("should get the correct sentences", function () {
             assert.deepEqual(sentences, [
               "iv. determining that the advertisement in the lift study is a candidate ad for the user, computing whether to include the user in a test group or a control group for the lift study ([0032]), v. based on the computation indicating that the user is in the control group, holding out the advertisement from completing the ad selection process for the user ([0032]), and vi. ",
               "based on the computation indicating that the user is in the test group, allowing the advertisement to continue through the ad selection process such that the user receives either the advertisement in the lift study or another advertisement ([0032]); and "
             ]);
+        });
+    });
+    describe('i.e.', function () {
+        var entry = "This can be particularly descriptive of the physical nature of the microheater, which can comprise an electrically conductive material that specifically can be provided in the form of a film—i.e., an electrically conductive layer. In certain embodiments, the electrically conductive material can be patterned. In other words, the electrically conductive material can be present in the microheater in a specific pattern.";
+        var sentences = tokenizer.sentences(entry, {preserve_whitespace: true, abbreviations: ['i[.]e']});
+
+        it("should get 3 sentences", function () {
+            assert.equal(sentences.length, 3);
+        });
+        it('sentences are correct', function () {
+            assert.equal(sentences.join(''), entry);
+            assert.equal(sentences[0], "This can be particularly descriptive of the physical nature of the microheater, which can comprise an electrically conductive material that specifically can be provided in the form of a film—i.e., an electrically conductive layer. ");
+            assert.equal(sentences[1], "In certain embodiments, the electrically conductive material can be patterned. ");
+            assert.equal(sentences[2], "In other words, the electrically conductive material can be present in the microheater in a specific pattern.");
         });
     });
 
